28c52aa2106e0b12d77e54bdea61d501cc6a58af
MarcosOliveira <mroliveira18041975@gmail.com>
2023-09-21 16:21:02 +0100

    update commit jest

--- rocketmovies-backend-main/ecosystem.config.js
+++ rocketmovies-backend-main/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'app',
-      script: './src/server.js', //nome do arquivo de inicialização
+      script: './src/server.js', //indica o nome do arquivo que irá iniciar a aplicação!
       instances: 'max',
       env: {
         NODE_ENV: 'development',
